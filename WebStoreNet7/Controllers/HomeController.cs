5773a61f00a78b94cfe919398854718743cc6215
Rueslan <krus1501@mail.ru>
2023-05-24 12:49:50 +0400

    Добавьте файлы проекта.

---
+++ WebStoreNet7/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace WebStoreNet7.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello from first controller");
+        }
+
+        public IActionResult SecondAction(string id)
+        {
+            return Content($"Second Action with parametr {id}");
+        }
+
+        public string NoIndex()
+        {
+            return "Hello from NoIndex";
+        }
+    }
+}
