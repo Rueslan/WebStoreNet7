736d7bf494fd6af03d1f14ae873933a9b3df0224
Rueslan <krus1501@mail.ru>
2023-05-24 14:23:18 +0400

    Создание списка сотрудников в контроллере

--- WebStoreNet7/Controllers/HomeController.cs
+++ WebStoreNet7/Controllers/HomeController.cs
@@ -1,10 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
+using WebStoreNet7.Models;
 
 namespace WebStoreNet7.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly List<Employee> _employees = new()
+        {
+            new Employee { Id = 1, FirstName = "Алексей", LastName = "Дударь", Patronymic = "Васильевич", Age = 25},
+            new Employee { Id = 2, FirstName = "Арсений", LastName = "Зайцев", Patronymic = "Сергеевич", Age = 12},
+            new Employee { Id = 3, FirstName = "Екатерина", LastName = "Козлова", Patronymic = "Олеговна", Age = 36}
+        };
+
         public IActionResult Index()
         {
             return View();
@@ -13,6 +21,10 @@ namespace WebStoreNet7.Controllers
         public IActionResult SecondAction(string id)
         {
             return Content($"Second Action with parametr {id}");
+        }
+        public IActionResult Employees()
+        {
+            return View(_employees);
         }
 
         public string NoIndex()
