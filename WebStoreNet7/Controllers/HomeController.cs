f111b35c5dafac8b343bb58dafc1598c0e6bf270
Rueslan <krus1501@mail.ru>
2023-05-24 15:13:55 +0400

    Добавление метода перехода на представление с дополнительной информацией о сотруднике, добавление информации о сотрудниках

--- WebStoreNet7/Controllers/HomeController.cs
+++ WebStoreNet7/Controllers/HomeController.cs
@@ -8,9 +8,9 @@ namespace WebStoreNet7.Controllers
     {
         private static readonly List<Employee> _employees = new()
         {
-            new Employee { Id = 1, FirstName = "Алексей", LastName = "Дударь", Patronymic = "Васильевич", Age = 25},
-            new Employee { Id = 2, FirstName = "Арсений", LastName = "Зайцев", Patronymic = "Сергеевич", Age = 12},
-            new Employee { Id = 3, FirstName = "Екатерина", LastName = "Козлова", Patronymic = "Олеговна", Age = 36}
+            new Employee { Id = 1, FirstName = "Алексей", LastName = "Дударь", Patronymic = "Васильевич", Age = 25, Info = "Бездельник"},
+            new Employee { Id = 2, FirstName = "Арсений", LastName = "Зайцев", Patronymic = "Сергеевич", Age = 12, Info = "Микро-стажёр"},
+            new Employee { Id = 3, FirstName = "Екатерина", LastName = "Козлова", Patronymic = "Олеговна", Age = 36, Info = "Волонтёр"}
         };
 
         public IActionResult Index()
@@ -25,6 +25,10 @@ namespace WebStoreNet7.Controllers
         public IActionResult Employees()
         {
             return View(_employees);
+        }
+        public IActionResult Info(int id)
+        {
+            return View(_employees[id-1]);
         }
 
         public string NoIndex()
