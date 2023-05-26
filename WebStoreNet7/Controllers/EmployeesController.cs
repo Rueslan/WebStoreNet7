e06568aec6ebb0a5b73be3086faf63df8617f77a
Rueslan <krus1501@mail.ru>
2023-05-26 13:57:56 +0400

    Переработка тестовых данных

--- WebStoreNet7/Controllers/EmployeesController.cs
+++ WebStoreNet7/Controllers/EmployeesController.cs
@@ -1,17 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStoreNet7.Data;
 using WebStoreNet7.Models;
 
 namespace WebStoreNet7.Controllers
 {
     public class EmployeesController : Controller
     {
-        private static readonly List<Employee> _employees = new()
+        private readonly IEnumerable<Employee> _Employees;
+        public EmployeesController()
         {
-            new Employee { Id = 1, FirstName = "Алексей", LastName = "Дударь", Patronymic = "Васильевич", Age = 25, Info = "Бездельник"},
-            new Employee { Id = 2, FirstName = "Арсений", LastName = "Зайцев", Patronymic = "Сергеевич", Age = 12, Info = "Микро-стажёр"},
-            new Employee { Id = 3, FirstName = "Екатерина", LastName = "Козлова", Patronymic = "Олеговна", Age = 36, Info = "Волонтёр"}
-        };
-
-        public IActionResult Index() => View(_employees);
+            _Employees = TestData.Employees;
+        }
+        public IActionResult Index() => View(_Employees);
     }
 }
---
+++ WebStoreNet7/Data/TestData.cs
@@ -0,0 +1,15 @@
+﻿using WebStoreNet7.Models;
+
+namespace WebStoreNet7.Data
+{
+    public static class TestData
+    {
+        private static readonly List<Employee> _employees = new()
+        {
+            new Employee { Id = 1, FirstName = "Алексей", LastName = "Дударь", Patronymic = "Васильевич", Age = 25, Info = "Бездельник"},
+            new Employee { Id = 2, FirstName = "Арсений", LastName = "Зайцев", Patronymic = "Сергеевич", Age = 12, Info = "Микро-стажёр"},
+            new Employee { Id = 3, FirstName = "Екатерина", LastName = "Козлова", Patronymic = "Олеговна", Age = 36, Info = "Волонтёр"}
+        };
+
+    }
+}
