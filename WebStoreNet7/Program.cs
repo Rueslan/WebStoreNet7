4b41eef265e36f252dd5080c36a64b01b505ae74
Rueslan <krus1501@mail.ru>
2023-05-28 20:01:28 +0400

    Добавление тестового промежуточного ПО(вызов)

--- WebStoreNet7/Program.cs
+++ WebStoreNet7/Program.cs
@@ -1,8 +1,13 @@
+using WebStoreNet7.Infrastructure.Convenctions;
+using WebStoreNet7.Infrastructure.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+builder.Services.AddControllersWithViews(opt => opt.Conventions.Add(new TestControllerConvention())).AddRazorRuntimeCompilation();
 var app = builder.Build();
 
 app.UseStaticFiles();
+app.UseRouting();
+app.UseMiddleware<TestMiddleware>();
 //app.MapGet("/", () => "Hello World!");
 app.MapControllerRoute(
     name: "default",
