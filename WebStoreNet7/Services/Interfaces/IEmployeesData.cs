2391cab6db16332e00494767fc388fc759189395
Rueslan <krus1501@mail.ru>
2023-05-28 20:04:47 +0400

    интерфейс сервиса сотрудников

---
+++ WebStoreNet7/Services/Interfaces/IEmployeesData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using WebStoreNet7.Models;
+
+namespace WebStoreNet7.Services.Interfaces
+{
+	public interface Interface
+	{
+		IEnumerable<Employee> GetAll();
+
+		Employee GetById(int id);
+
+		int Add(Employee employee);
+
+		void Update(Employee employee);
+
+		bool Delete(int id);
+	}
+}
