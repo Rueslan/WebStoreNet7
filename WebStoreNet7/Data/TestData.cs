aa2d5a9ca819f6280c1da16419e54b335390f364
Rueslan <krus1501@mail.ru>
2023-06-02 08:18:03 +0400

    Валидация данных модели

--- WebStoreNet7/Data/TestData.cs
+++ WebStoreNet7/Data/TestData.cs
@@ -7,7 +7,7 @@ namespace WebStoreNet7.Data
         public static List<Employee> Employees { get; } = new()
         {
             new Employee { Id = 1, FirstName = "Алексей", LastName = "Дударь", Patronymic = "Васильевич", Age = 25, Info = "Бездельник"},
-            new Employee { Id = 2, FirstName = "Арсений", LastName = "Зайцев", Patronymic = "Сергеевич", Age = 12, Info = "Микро-стажёр"},
+            new Employee { Id = 2, FirstName = "Арсений", LastName = "Зайцев", Patronymic = "Сергеевич", Age = 22, Info = "Микро-стажёр"},
             new Employee { Id = 3, FirstName = "Екатерина", LastName = "Козлова", Patronymic = "Олеговна", Age = 36, Info = "Волонтёр"}
         };
 
--- WebStoreNet7/ViewModels/EmployeeViewModel.cs
+++ WebStoreNet7/ViewModels/EmployeeViewModel.cs
@@ -3,24 +3,50 @@ using System.ComponentModel.DataAnnotations;
 
 namespace WebStoreNet7.ViewModels
 {
-	public class EmployeeViewModel
+	public class EmployeeViewModel : IValidatableObject
 	{
 		[HiddenInput(DisplayValue =false)]	
 		public int Id { get; set; }
 
 		[Display(Name = "Имя")]
+        [Required(ErrorMessage ="Имя не указано")]
+        [StringLength(200,MinimumLength =2,ErrorMessage = "Допустимое количество символов от 2 до 200")]
+        [RegularExpression(@"([А-ЯЁ][а-яё]+)|([A-Z][a-z]+)", ErrorMessage = "Неверный формат")]
 		public string? Name { get; set; }
 
         [Display(Name = "Фамилия")]
+        [Required(ErrorMessage = "Фамилия не указана")]
+        [StringLength(200, MinimumLength = 2, ErrorMessage = "Допустимое количество символов от 2 до 200")]
+        [RegularExpression(@"([А-ЯЁ][а-яё]+)|([A-Z][a-z]+)", ErrorMessage = "Неверный формат")]
         public string? LastName { get; set; }
 
         [Display(Name = "Отчество")]
+        [StringLength(200, MinimumLength = 2, ErrorMessage = "Допустимое количество символов от 2 до 200")]
         public string? Patronymic { get; set; }
 
         [Display(Name = "Возраст")]
+        [Required(ErrorMessage = "Возраст не указан")]
+        [Range(18, 80, ErrorMessage = "от 18 до 80")]
         public int Age { get; set; }
 
         [Display(Name = "Дополнительно")]
         public string? Info { get; set; }
-	}
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            switch (validationContext.MemberName)
+            {
+                default:
+                    // return Enumerable.Empty<ValidationResult>();
+                    return new[] { ValidationResult.Success };
+
+                    case nameof(Age):
+                    if (Age < 18 || Age > 80)
+                    {
+                        return new[] { new ValidationResult("Странный возраст")};
+                    }
+                    return new[] { ValidationResult.Success };
+            }
+        }
+    }
 }
